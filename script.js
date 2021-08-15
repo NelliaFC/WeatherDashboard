769f2c8d6ff0043345381a948de411bcab5e6b41
NeoNeonilla <nelliafleurova@gmail.com>
2021-08-15 15:49:52 -0500

    created functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function searchCity(){
 var searchValue= document.querySelector("#search-value").value
 weatherApi(searchValue)
-  console.log(searchValue)
+  
 }
 document.getElementById("search-button").addEventListener("click", searchCity)
 
@@ -11,23 +11,72 @@ function weatherApi (searchValue){
   .then(data => {
     var todayDiv = document.querySelector("#today")
 
+//clear content
+    todayDiv.textContent = ""
+
+var parentDiv= document.createElement("div")
+parentDiv.classList.add("card")
+
     var cityName = document.createElement("h2")
-    cityName.textContent= `${data.name}`
+    cityName.textContent= `City name: ${data.name}`
+    cityName.classList.add("card-text")
+
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src",`http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
+   
 
      var temp = document.createElement("p")
      temp.textContent= `temp: ${data.main.temp}`
 
    var humidity = document.createElement("h4")
-   humidity.textContent=`${data.main.humidity}`
+   humidity.textContent=`humidity: ${data.main.humidity}`
 
-  var uvIndex = document.createElement("h4")
-  // uvIndex.textContent = `${data.}`// work on that one with another API call
     var windSpeed = document.createElement("h4")
-    windSpeed.textContent= `${data.wind.speed}`
-    
-    todayDiv.append(cityName, temp, humidity, windSpeed)
-    
+    windSpeed.textContent= `Wind Speed: ${data.wind.speed}`
+    parentDiv.append(cityName, weatherIcon, temp, humidity, windSpeed)
+
+
+
+    todayDiv.append(parentDiv)
+    oneCallUv(data.coord.lat, data.coord.lon)
     console.log(data)});
- 
+
 }
 
+function oneCallUv (lat, lon){
+  fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=a21bd5083b0e1585843e6ff136f49bca")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    var todayDiv = document.querySelector("#today")
+    var todayUv = document.createElement("h2")
+    todayUv.textContent= `uv index: ${data.current.uvi}`
+for(var i=1; i< data.daily.length-2; i++){
+
+var forecastDiv = document.querySelector("#forecast")
+
+  var forecastTemp = document.createElement("h4")
+  forecastTemp.textContent= `${data.daily[i].temp.day}`
+
+  var forecastHumid = document.createElement("h4")
+  forecastHumidity.textContent= `${data.}`
+//create elements for each one
+var forecastWind = document.createElement("h4")
+var forecastUv = document.createElement("h4")
+var forecastIcon = document.createElement("h4")
+
+
+
+forecastDiv.append(forecastTemp, forecastWind, forecastHumid,forecastUv,forecastIcon)
+
+}
+
+todayDiv.append(todayUv);
+  })
+  
+}
+
+
+// to clear the old content to show new content use
+// repoContainerEl.textContent = "";
+// repoSearchTerm.textContent = searchTerm;
